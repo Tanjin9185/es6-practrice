a13aa0ab7a3ee64b3083cb3cbae3823f07d4aaa9
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-01-31 23:03:59 +0600

    done

---
+++ letconst.js
@@ -0,0 +1,21 @@
+const hubby = "Elias Kanchon";
+//hubby = "omar sani"      //const variable can not be change
+console.log(hubby);
+
+const number = [12, 45];
+number[1] = 88;
+number.push(12);
+//number = ["omar", "sunny"];         //full array chnge psbl na..num array string hbe na
+console.log(number);
+
+const nayok = {name:"sakib", phone:7879};  //object a o vaule update kra jabe..but variable cannot be replace
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];    
+}
+//console.log(i);   //loop a variable let kre dile baire lek hoi na
