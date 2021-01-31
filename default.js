b5695b51d6404fbcad1a3c9d623fb74d9ffff776
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-01-31 23:16:31 +0600

    ok

---
+++ default.js
@@ -0,0 +1,8 @@
+function add(num1, num2, num3, num4 = 0){
+    return num1 + num2 + num3 + num4;
+}
+console.log(add(15,1,1));
+
+//vule parameter pass korte vule gele function er parameter a  = 0 dye dyai last parameter 0 dre nibe...only last ta dre nibe
+
+//eke function parameter backup o bola hoi
