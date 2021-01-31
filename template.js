8eb48464bee2539be4ea1eace3a0f2000aae52d9
Tanjin Ahamed <tanjin15-9185@diu.edu.bd>
2021-01-31 23:35:23 +0600

    ok

---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = "Tanjin";
+const lastName = "Ahamed";
+const fullName = firstName + " " + lastName + " is a good boy.";
+const fullName1 = `${firstName} ${lastName} ${10+10+10} is a good boy`;
+
+console.log(fullName);
+console.log(fullName1);
+
+const multiLine = "I love you\n"
++ "I miss you\n" + "I need you";
+const multiLine2 = `I love you  I miss you
+I need you`
+console.log(multiLine);
+console.log(multiLine2);
